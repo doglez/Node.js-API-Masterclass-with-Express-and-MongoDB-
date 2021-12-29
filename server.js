27db8fe8993e1965814998dc50212bbf2f4c46c1
doglez <danilo.j.gonzalez@gmail.com>
2021-12-28 21:20:22 -0600

    creacion de proyecto

---
+++ server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config({ path: "./.env" });
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port http://localhost:${PORT}`
+  )
+);
