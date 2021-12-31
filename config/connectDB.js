1afd0a38e003e819ce9810d2db47704e57d3a946
doglez <danilo.j.gonzalez@gmail.com>
2021-12-30 20:18:24 -0600

    coneccion con la db con mongoose

---
+++ config/connectDB.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  const conn = await mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  console.log(`MongoDB connected: ${conn.connection.host}`);
+};
+
+export default connectDB;
--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import morgan from "morgan";
+import connectDB from "./config/connectDB.js";
 import bootcampsRouter from "./routes/bootcampsRouter.js";
 import logger from "./middleware/logger.js";
 
 dotenv.config({ path: "./.env" });
+
+// Connecto to DB
+connectDB();
 
 const app = express();
 
@@ -17,9 +21,15 @@ app.use("/api/v1/bootcamps", bootcampsRouter);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(
     `Server running in ${process.env.NODE_ENV} mode on port http://localhost:${PORT}`
   )
 );
+
+// Handle unhandle promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  server.close(() => process.exit(1));
+});
